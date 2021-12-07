b08cc3e57cc4883b0632439ab403c5c0cf4545e2
Panin <panin1970@gmail.com>
2021-12-07 18:15:39 +0300

    Finish Home Work Two Views

--- TwoViews/ViewController.swift
+++ TwoViews/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var redView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
